13d56935936ce78cd9f6cf689e82be38c02afaa9
Ciprian Miclaus <ciprianm@gmail.com>
2023-09-18 21:30:49 +0100

    Add doc strings to the RulesBlock

--- finrules/backends/common/basic.py
+++ finrules/backends/common/basic.py
@@ -30,6 +30,29 @@ class BaseProjectRule(BaseRule):
 
 
 class RulesBlock(BaseRule):
+    """ Groups rules into encapsulated blocks or units of rules that achieve one thing.
+    Blocks are reusable and encapsulated to reduce complexity.
+
+    Params:
+        rules: An iterable of rules which are part of this block.
+            The first rule in the block will take its input from the named_input of the RulesBlock (if any, if not from the main output of the previous rule).
+            The last rule in the block will publish the output as the named_output of the RulesBlock (if any, or the main output of the block).
+            Any named outputs in the block are not exposed to the rules outside of the block (proper encapsulation).
+
+    Common params:
+        named_input: Which dataframe to use as the input. Optional.
+            When not set, the input is taken from the main output.
+            Set it to a string value, the name of an output dataframe of a previous rule.
+        named_output: Give the output of this rule a name so it can be used by another rule as a named input. Optional.
+            When not set, the result of this rule will be available as the main output.
+            When set to a name (string), the result will be available as that named output.
+        name: Give the rule a name. Optional.
+            Named rules are more descriptive as to what they're trying to do/the intent.
+        description: Describe in detail what the rules does, how it does it. Optional.
+            Together with the name, the description acts as the documentation of the rule.
+        strict: When set to True, the rule does a stricter valiation. Default: True
+    """
+
     def __init__(self, rules, named_input=None, named_output=None, name=None, description=None, strict=True):
         self._rules = [rule for rule in rules]
         assert self._rules, "RulesBlock: Empty rules set provided."
@@ -39,6 +62,17 @@ class RulesBlock(BaseRule):
         super().__init__(named_input=named_input, named_output=named_output, name=name, description=description, strict=strict)
 
     def apply(self, data):
+        """ Applies the rule to the input data.
+
+        Params:
+            data: An instance of RuleData which stores inputs and outputs, including the main outputs and any named inputs/outputs.
+
+        Returns:
+            None
+
+        Note:
+            apply doesn't return any data but it sets the results on the input data parameter (either main output or a named output depending on the rule set up).
+        """
         data2 = RuleData(
             main_input=self._get_input_df(data),
             named_inputs={k: v for k, v in data.get_named_outputs()},
