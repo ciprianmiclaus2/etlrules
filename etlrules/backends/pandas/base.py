125415c36a274e20422f8fdcc054fcda19ee3c6d
Ciprian Miclaus <ciprianm@gmail.com>
2023-10-12 16:20:39 +0100

    Remove unused code.

--- etlrules/backends/pandas/base.py
+++ etlrules/backends/pandas/base.py
@@ -21,20 +21,3 @@ class BaseAssignColumnRule(UnaryOpBaseRule, ColumnsInOutMixin):
         input_column, output_column = self.validate_in_out_columns(df, self.input_column, self.output_column, self.strict)
         df = df.assign(**{output_column: self.do_apply(df, df[input_column])})
         self._set_output_df(data, df)
-
-
-class BaseAssignRule(UnaryOpBaseRule, ColumnsInOutMixin):
-
-    def __init__(self, columns: Iterable[str], output_columns:Optional[Iterable[str]]=None, named_input: Optional[str]=None, named_output: Optional[str]=None, name: Optional[str]=None, description: Optional[str]=None, strict: bool=True):
-        super().__init__(named_input=named_input, named_output=named_output, name=name, description=description, strict=strict)
-        self.columns = [col for col in columns]
-        self.output_columns = [out_col for out_col in output_columns] if output_columns else None
-
-    def do_apply(self, col):
-        raise NotImplementedError
-
-    def apply(self, data):
-        df = self._get_input_df(data)
-        columns, output_columns = self.validate_columns_in_out(df, self.columns, self.output_columns, self.strict)
-        df = df.assign(**{output_col: self.do_apply(df[col]) for col, output_col in zip(columns, output_columns)})
-        self._set_output_df(data, df)
