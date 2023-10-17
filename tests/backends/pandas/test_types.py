f5e02dc13c36c818731a093c006b0c6b324a9fa8
Ciprian Miclaus <ciprianm@gmail.com>
2023-10-17 15:10:56 +0100

    Empty df type test

--- tests/backends/pandas/test_types.py
+++ tests/backends/pandas/test_types.py
@@ -12,20 +12,20 @@ from tests.backends.pandas.utils.data import get_test_data
         {'A': '1', 'B': 2},
         {'A': '2', 'B': 3},
         {'A': '3', 'B': 4},
-    ]), {'A': 'int64', 'B': 'str'}, DataFrame(data=[
+    ]), {'A': 'int64', 'B': 'string'}, DataFrame(data=[
         {'A': 1, 'B': '2'},
         {'A': 2, 'B': '3'},
         {'A': 3, 'B': '4'},
-    ])],
+    ]).astype({"A": "Int64", "B": "string"})],
     [DataFrame(data=[
         {'A': '1.5', 'B': 2.0},
         {'A': '2.0', 'B': 3.45},
         {'A': '3.45', 'B': 4.5},
-    ]), {'A': 'float64', 'B': 'str'}, DataFrame(data=[
+    ]), {'A': 'float64', 'B': 'string'}, DataFrame(data=[
         {'A': 1.5, 'B': '2.0'},
         {'A': 2.0, 'B': '3.45'},
         {'A': 3.45, 'B': '4.5'},
-    ])],
+    ]).astype({"B": "string"})],
     [DataFrame(data=[
         {'A': 1, 'B': 2.0},
         {'A': 2, 'B': 3.0},
@@ -34,7 +34,19 @@ from tests.backends.pandas.utils.data import get_test_data
         {'A': 1.0, 'B': 2},
         {'A': 2.0, 'B': 3},
         {'A': 3.0, 'B': 4},
-    ])],
+    ]).astype({"B": "Int64"})],
+    [DataFrame(data=[
+        {'A': 1, 'B': "test"},
+        {'B': "do"},
+        {'A': 3, 'B': "ex"},
+    ]), {'A': 'int64', 'B': 'object'}, DataFrame(data=[
+        {'A': 1, 'B': "test"},
+        {'B': "do"},
+        {'A': 3, 'B': "ex"},
+    ]).astype({"A": "Int64", "B": "object"})],
+    [DataFrame(data={"A": [], "B": []}).astype({"A": "string", "B": "int64"}),
+        {'A': 'int64', 'B': 'string'},
+        DataFrame(data={"A": [], "B": []}).astype({"A": "Int64", "B": "string"})],
 ])
 def test_type_conversion_rule_scenarios(input_df, conversion_dict, expected_df):
     with get_test_data(input_df, named_inputs={"other": input_df}, named_output="result") as data:
@@ -52,7 +64,7 @@ def test_type_conversion_rule_missing_column():
     with get_test_data(df, named_inputs={"other": df}, named_output="result") as data:
         rule = TypeConversionRule({
             'A': 'int64',
-            'C': 'str',
+            'C': 'string',
         }, named_output="result")
         with pytest.raises(MissingColumnError):
             rule.apply(data)
