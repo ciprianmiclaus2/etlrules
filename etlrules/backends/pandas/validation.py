f60016b062c740bf2e0dbddc9a3f7fa20543e9f8
Ciprian Miclaus <ciprianm@gmail.com>
2023-11-02 07:52:48 +0000

    Remove low value, unused PandasValidation class

--- etlrules/backends/pandas/validation.py
+++
@@ -1,6 +0,0 @@
-from pandas import DataFrame
-
-
-class PandasRuleValidationMixin:
-    def assert_is_dataframe(self, df: DataFrame, context: str):
-        assert isinstance(df, DataFrame), context
