62ae4ec6db7c64664821983fcb8325b788bdc8bf
Ciprian Miclaus <ciprianm@gmail.com>
2023-11-10 22:39:51 +0000

    Skip polars weekday tests on 3.9 and lower as polars_business doesn't support py3.9 or lower

--- tests/rules/test_datetime.py
+++ tests/rules/test_datetime.py
@@ -1,5 +1,7 @@
 import datetime
+import polars as pl
 import pytest
+import sys
 
 from etlrules.exceptions import ColumnAlreadyExistsError, MissingColumnError
 from tests.utils.data import assert_frame_equal, get_test_data
@@ -665,6 +667,8 @@ INPUT_ADD_SUB_DF4 = [
     ["DateTimeSubstractRule", "A", 10, "days", "B", INPUT_ADD_SUB_DF2, None, ColumnAlreadyExistsError, None],
     ["DateTimeSubstractRule", "A", "C", "days", None, INPUT_ADD_SUB_DF2, None, MissingColumnError, None],])
 def test_add_sub_rules(rule_cls_str, input_column, unit_value, unit, output_column, input_df, input_astype, expected, expected_astype, backend):
+    if backend.impl == pl and unit_value == "weekdays" and sys.version_info <= (3, 10):
+        pytest.skip()
     input_df = backend.DataFrame(input_df, astype=input_astype)
     if isinstance(expected, (list, dict)):
         expected = backend.DataFrame(expected, astype=expected_astype)
