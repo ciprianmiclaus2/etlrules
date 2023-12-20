d0ff0ed50a03a2be476240b0c196db8f9f250dc8
Ciprian Miclaus <ciprianm@gmail.com>
2023-12-20 12:34:20 +0000

    Remove polar business from thr datetime rules

--- etlrules/backends/polars/datetime.py
+++ etlrules/backends/polars/datetime.py
@@ -1,10 +1,6 @@
 import datetime
 import locale
 import polars as pl
-try:
-    import polars_business as plb
-except:
-    plb = None
 
 from .base import PolarsMixin
 from etlrules.exceptions import ColumnAlreadyExistsError, MissingColumnError
@@ -290,9 +286,7 @@ def add_sub_col(df, col, unit_value, unit, sign, input_column, strict=True):
     if unit not in DT_ARITHMETIC_UNITS.keys():
         raise ValueError(f"Unsupported unit: '{unit}'. It must be one of {DT_ARITHMETIC_UNITS.keys()}")
     if unit == "weekdays":
-        if plb is None:
-            raise RuntimeError("Calculation requires polars_business. pip install polars_business.")
-        return plb.col(input_column).bdt.offset_by(f"{sign * unit_value}{OFFSETS[unit]}", weekend=('Sat', 'Sun'), roll="forward" if sign == -1 else "backward")
+        return col + business_day_offset(col, sign * df[unit_value], strict=strict)
     return col.dt.offset_by(f"{sign * unit_value}{OFFSETS[unit]}")
 
 
